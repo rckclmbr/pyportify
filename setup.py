74d5ccb64d2964185d8c4b9e8ce454c529243129
Josh Braegger <rckclmbr@gmail.com>
2015-01-12 02:49:39 -0700

    Include license

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(requirements_txt) as reqs:
 
 setup(
     name='pyportify',
-    version="0.1.4",
+    version="0.1.5",
     author='Josh Braegger',
     author_email='rckclmbr@gmail.com',
     packages=find_packages(),
